b3e23f2ba1b6f364cf2404b0370bb9ab72a503d6
vladops <vlad123123v@gmail.com>
2024-01-30 22:17:57 +0300

    Update createToken.ts

--- token/createToken.ts
+++ token/createToken.ts
@@ -1,26 +1,38 @@
 // $FUTURE
-// 
+//
 // Supply - 1,000,000 $FUTURE
-// 
+//
 // Tokenomics:
 //  - 5% Team
 //  - 5% Marketing
-//  - 90% Liquidity pool 
+//  - 90% Liquidity pool
 //
 // Features:
 //  - Every day, the liquidity pool is replenished
 //    and burned by 0.1% (900) $FUTURE
 
-import { readFileSync, writeFileSync } from 'fs';
-import { join } from 'path';
+import { readFileSync, writeFileSync } from "fs";
+import { join } from "path";
 
-import * as Solana from '@solana/web3.js' 
-import * as SolanaToken from '@solana/spl-token' 
+import * as Solana from "@solana/web3.js";
+import * as SolanaToken from "@solana/spl-token";
 
+const SOLANA_RPC_NAME = "devnet";
+const SOLSCAN_URI = "https://solscan.io/{}/{}?cluster={}";
 
-const SOLANA_RPC_NAME = "devnet"
-const SOLSCAN_URI = "https://solscan.io/{}/{}?cluster={}"
-const EPOCH_TIMESTAMP = Date().valueOf()
+const FUTURE_TOTAL_SUPPLY = 1_000_000;
+const FUTURE_TOKENOMICS_TEAM = (FUTURE_TOTAL_SUPPLY / 100) * 5;
+const FUTURE_TOKENOMICS_MARKETING = (FUTURE_TOTAL_SUPPLY / 100) * 5;
+const FUTURE_TOKENOMICS_LIQUIDITY_POOL = (FUTURE_TOTAL_SUPPLY / 100) * 90;
+
+if (
+    FUTURE_TOTAL_SUPPLY !==
+    FUTURE_TOKENOMICS_TEAM +
+        FUTURE_TOKENOMICS_MARKETING +
+        FUTURE_TOKENOMICS_LIQUIDITY_POOL
+) {
+    throw Error("Total supply doesnt match with tokenomics");
+}
 
 const tokenCreator = Solana.Keypair.generate();
 const mintAuthority = Solana.Keypair.generate();
@@ -29,37 +41,37 @@ const tokenTeam = Solana.Keypair.generate();
 const tokenMarketing = Solana.Keypair.generate();
 
 writeFileSync(
-    join(__dirname, `FUTURE-${EPOCH_TIMESTAMP}`),
+    join(__dirname, `FUTURE-${Date().valueOf()}`),
     {
-        "addresses": [
+        addresses: [
             {
-                "name": "tokenCreator",
-                "publicKey": tokenCreator.publicKey.toBase58(),
-                "secretKey": tokenCreator.secretKey
+                name: "tokenCreator",
+                publicKey: tokenCreator.publicKey.toBase58(),
+                secretKey: tokenCreator.secretKey
             },
             {
-                "name": "mintAuthority",
-                "publicKey": mintAuthority.publicKey.toBase58(),
-                "secretKey": mintAuthority.secretKey
+                name: "mintAuthority",
+                publicKey: mintAuthority.publicKey.toBase58(),
+                secretKey: mintAuthority.secretKey
             },
             {
-                "name": "freezeAuthority",
-                "publicKey": freezeAuthority.publicKey.toBase58(),
-                "secretKey": freezeAuthority.secretKey
+                name: "freezeAuthority",
+                publicKey: freezeAuthority.publicKey.toBase58(),
+                secretKey: freezeAuthority.secretKey
             },
             {
-                "name": "tokenTeam",
-                "publicKey": tokenTeam.publicKey.toBase58(),
-                "secretKey": tokenTeam.secretKey
+                name: "tokenTeam",
+                publicKey: tokenTeam.publicKey.toBase58(),
+                secretKey: tokenTeam.secretKey
             },
             {
-                "name": "tokenMarketing",
-                "publicKey": tokenMarketing.publicKey.toBase58(),
-                "secretKey": tokenMarketing.secretKey
-            },
+                name: "tokenMarketing",
+                publicKey: tokenMarketing.publicKey.toBase58(),
+                secretKey: tokenMarketing.secretKey
+            }
         ]
     },
-    {flag: 'w'}
+    { flag: "w" }
 );
 
 console.log(`Solana $FUTURE token creator (${SOLANA_RPC_NAME})\n\n`);
@@ -75,12 +87,15 @@ console.log(`Token team\n
 console.log(`Token marketing\n
              Public key: ${tokenMarketing.publicKey.toBase58()}`);
 
-const connection = new Solana.Connection(Solana.clusterApiUrl(SOLANA_RPC_NAME), 'confirmed');
+const connection = new Solana.Connection(
+    Solana.clusterApiUrl(SOLANA_RPC_NAME),
+    "confirmed"
+);
 
 if (SOLANA_RPC_NAME == "devnet") {
     const airdropSignature = await connection.requestAirdrop(
         tokenCreator.publicKey,
-        Solana.LAMPORTS_PER_SOL,
+        Solana.LAMPORTS_PER_SOL
     );
     await connection.confirmTransaction(airdropSignature);
 }
